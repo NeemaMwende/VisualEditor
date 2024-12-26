bd7da16c53e488b6d6a679d5dae3fb370f82ba37
Neema <neemamwende009@gmail.com>
2024-12-26 13:12:26 +0300

    build updates

--- src/app/questioneditor/page.tsx
+++ src/app/questioneditor/page.tsx
@@ -1,16 +1,15 @@
-import QuestionEditor, {Question} from './components/QuestionEditor';
-//import { Question } from './components/QuestionEditor';
+"use client"
+import QuestionEditor, { Question } from './components/QuestionEditor';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 p-8">
       <QuestionEditor 
         onSave={(data: Question) => {
-          
           console.log(data);  
         }} 
         onBack={() => {
-         console.log("this is a placeholder for the back button");
+          console.log("this is a placeholder for the back button");
         }} 
       />
     </main>
